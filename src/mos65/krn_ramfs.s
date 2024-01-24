fa1c41add757f06ae8d10a06ffdc2dd417cfe2aa
Alex230805 <misuralealessandro@gmail.com>
2024-01-24 22:25:10 +0100

    malloc re-implementation

--- src/mos65/krn_ramfs.s
+++ src/mos65/krn_ramfs.s
@@ -33,8 +33,8 @@ __K_RAMS:
 
 __ram_malloc:
     clv ; clear overflow
-    tya ; save y content
-    pha ; into stack
+    tya 
+    pha
 
     lda RAM_FREE_PAGE           ; load ram free page
     cmp #$00                    ; compare with 0
@@ -49,127 +49,147 @@ __ram_malloc:
     clc                         ; clear carry
     pla                         ; pull y content
     pha                         ; and re-save it
-    cmp RAM_FREE_PAGE           ; compare with the free page counter
-    bcc __ram_malloc_page_disp  ; check if is less
-    bcs __ram_out_of_request    ; check if the request is greater than free pages
+
+
+    __ram_malloc_check_for_free_page:
+        ldx #$00
+        lda RAM_PAGE_ALLOC_POINTER,x
+        cmp #$00
+        beq __ram_malloc_check_for_free_page_y_index
+        bne __ram_out_of_request
+
+        __ram_malloc_check_for_free_page_y_index:
+            clc
+            stx RAM_LB_CALCULUS_ADDRESS
+            cpy RAM_LB_CALCULUS_ADDRESS
+            bcc __ram_malloc_check_desired_amount
+            bcs __ram_malloc_check_for_free_page 
+
+            __ram_malloc_check_desired_amount:
+                clc
+                clv
+                sty RAM_LB_CALCULUS_ADDRESS
+                cpx RAM_LB_CALCULUS_ADDRESS
+                beq __ram_malloc_page_disp
+                bne __ram_malloc_continue_to_check_for_page
+
+                __ram_malloc_continue_to_check_for_page:
+                    jmp __ram_malloc_check_for_free_page
 
     __ram_out_of_request:       ; out of request -> abort operation
         jmp __ram_end_malloc
 
     __ram_malloc_page_disp:
 
-    adc RAM_ALLOCATED_PAGE      ; update allocated page counter
-    sta RAM_ALLOCATED_PAGE      ; store it 
+        adc RAM_ALLOCATED_PAGE      ; update allocated page counter
+        sta RAM_ALLOCATED_PAGE      ; store it 
 
-    lda RAM_FREE_PAGE           ; load free counter
-    sec                         ; set carry
-    sbc RAM_ALLOCATED_PAGE      ; subtract with allocated page counter
-    sta RAM_FREE_PAGE           ; save free page counter
+        lda RAM_FREE_PAGE           ; load free counter
+        sec                         ; set carry
+        sbc RAM_ALLOCATED_PAGE      ; subtract with allocated page counter
+        sta RAM_FREE_PAGE           ; save free page counter
 
 
-    ldx RAM_PAGE_ALLOC_POINTER  ; load current ram vector flag pointer
+        ldx RAM_PAGE_ALLOC_POINTER
+        stx RAM_LB_CALCULUS_ADDRESS     ; save for later
 
-    clc                         ; clear carrt
-    lda RAM_PAGE_ALLOC_POINTER         ;  Assiign progressive id to any program in memory
-    adc #"A"
-    cmp #$00                    ; check for error
-    bvs __out_of_memory_error
-    bvc __ram_malloc_assing_vector
+        lda RAM_PAGE_ALLOC_POINTER
+        adc #"A"
+        
+        sta RAM_ALLOCATED_ASSINGED_LETTER
 
-    __out_of_memory_error:
-        lda #KERNEL_OUT_OF_MEMORY_ERROR     ; kernel panic -> out of memory error
-        sta KERNEL_STATE
-        jmp __kenrel_panic
+        clv
+        clc
 
-    __ram_malloc_assing_vector:
-        sta RAM_ALLOCATED_ASSINGED_LETTER    ; assing letter
-    __ram_malloc_start_alloc:               
-        cpy #$00                            ;  like "while() do {} function"
-        beq __end_flag_allocation
-        bne __ram_malloc_assing_vector_continue
-
-        __ram_malloc_assing_vector_continue:
+        __ram_malloc_assign_char_vector:
             sta RAM_PAGE_ALLOCATED_FLAG_HEAD,x
-            dey
             inx
-            jmp __ram_malloc_start_alloc
+            dey
+            cpy #$00
+            beq __ram_malloc_end_char_assigment
+            bne __ram_malloc_assign_char_vector
 
-        __end_flag_allocation:
-
-            txy
-
-            pla ; pull  from stack and put it into a
+        __ram_malloc_end_char_assigment:
             
-            sta RAM_RETURNED_ALLOCATED_LAST_POINTER_LB ; just store it for a moment
+            stx RAM_PAGE_ALLOC_POINTER
 
-            sta RAM_PAGE_ALLOC_POINTER  ; use this store cache data
-            tya 
-            sec ; set carry flag
-            sbc RAM_PAGE_ALLOC_POINTER 
+            ldx RAM_LB_CALCULUS_ADDRESS
 
-            sty RAM_PAGE_ALLOC_POINTER  ; store the real page allocated pointer
-
-            clc
-            clv
-
-            tax ; transfer the original starting point
-            pha ; for stay safe it's better to save the original starting point
-
-
-            ; save pointer
-            lda RAM_PAGE_POINTER_VECTOR_HEAD,x
+            lda RAM_PAGE_PADDING_HEAD,x
             sta RAM_RETURNED_ALLOCATED_FIRST_POINTER_LB
             inx
-            lda RAM_PAGE_POINTER_VECTOR_HEAD,x
+            lda RAM_PAGE_PADDING_HEAD,x
             sta RAM_RETURNED_ALLOCATED_FIRST_POINTER_HB
-            inx
+            dex
 
+            clc
             pla 
+            adc #$RAM_LB_CALCULUS_ADDRESS
+            asl
             tax
-            lda RAM_RETURNED_ALLOCATED_LAST_POINTER_LB  ; grab the saved index
-            asl             ; multiply by 2 
-            bvs __ram_malloc_error_out_of_memory_index
-            bvc __ram_malloc_calculate_index
+            dex
 
-            __ram_malloc_error_out_of_memory_index:
-                lda #MALLOC_OUT_OF_MEMORY_INDEX
-                sta KERNEL_STATE
-                jmp __kenrel_panic
-
-            __ram_malloc_calculate_index:
-                sec
-                sbc #$01
-
-                clc
-                clv
-
-                stx RAM_RETURNED_ALLOCATED_LAST_POINTER_LB ; store the x index just for now here
-                adc RAM_RETURNED_ALLOCATED_LAST_POINTER_LB  ; calculate the index of 
-                
-                bvs __ram_malloc_error_out_of_memory_index
-                bvc __ram_malloc_get_pointer_by_index
-
-                __ram_malloc_get_pointer_by_index:
-
-                    tax ; transfer the resoult index into x
-
-
-                    ; save pointer
-                    lda RAM_PAGE_POINTER_VECTOR_HEAD,x
-                    sta RAM_RETURNED_ALLOCATED_LAST_POINTER_LB
-                    inx
-                    lda RAM_PAGE_POINTER_VECTOR_HEAD,x
-                    sta RAM_RETURNED_ALLOCATED_LAST_POINTER_HB
+            lda RAM_PAGE_PADDING_HEAD,x
+            sta RAM_RETURNED_ALLOCATED_LAST_POINTER_LB
+            inx
+            lda RAM_PAGE_PADDING_HEAD,x
+            sta RAM_RETURNED_ALLOCATED_LAST_POINTER_HB
 
     __ram_end_malloc:
         rts 
 
 
 __ram_free:
+
     rts
 
 
 
 __k_ram_init_set_page_address_table:
+    ldy #$50
+    ldx #$00
+    clc
+    lda #$00
+    sta RAM_LB_CALCULUS_ADDRESS
+    lda #$40
+    sta RAM_HB_CALCULUS_ADDRESS
 
+
+
+    __k_ram_offset_by_one:
+        lda RAM_LB_CALCULUS_ADDRESS
+        clc
+        adc #$ff
+        sta RAM_LB_CALCULUS_ADDRESS
+        adc #$00
+        sta RAM_HB_CALCULUS_ADDRESS
+
+    __k_ram_calculate_offset:
+
+        lda RAM_LB_CALCULUS_ADDRESS
+        sta RAM_PAGE_PADDING_HEAD,x
+        inx
+        lda RAM_HB_CALCULUS_ADDRESS
+        sta RAM_PAGE_PADDING_HEAD,x
+        inx
+
+        lda RAM_LB_CALCULUS_ADDRESS
+        clc
+        adc #$ff
+        sta RAM_LB_CALCULUS_ADDRESS
+        adc #$00
+        sta RAM_HB_CALCULUS_ADDRESS
+        inx
+
+
+        dey
+
+        cpy #$00
+        beq __k_ram_end_init_page
+        bne __k_ram_continue_calculate_offset
+
+        __k_ram_continue_calculate_offset:
+            jmp __k_ram_offset_by_one
+        
+        __k_ram_end_init_page:
     rts                             ; return to initialization
