9d08577cc72d06b56580a68c93fe29ec588d564a
Alex230805 <misuralealessandro@gmail.com>
2024-02-26 16:46:57 +0100

    dynamic header code doc added

--- src/doc/software/zenith/main.c
+++ src/doc/software/zenith/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
 
 #define ZENITH_IMPLEMENTATION
+
+    - init_node fn
+    - init_data_node fn
+    - init_folder fn
+    - zenith malloc fn
+
 #define ZENITH_MKFS_IMPLEMENTATION
+
+    - zenith_init_fs fn
+
+**/
+
 #include "include/zenith.h"
 
 int main(void){
